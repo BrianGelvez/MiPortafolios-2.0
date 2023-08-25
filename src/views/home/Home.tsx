91d4df25f34dfd96f3368e71a38238e80132d3af
Brian Gelvez <briannn97@gmail.com>
2023-08-25 16:26:37 -0300

    ahora si

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./home.module.css";
 import pin from '../../assets/pin.svg'
 import message from '../../assets/message.svg'
@@ -7,33 +7,42 @@ import Buttons from "../../components/buttoons/Buttons";
 import AnimacionOne from "../../components/animations/animationOne/AnimationOne";
 
 const Home: React.FC = () => {
-  
+  const [isResponsive, setIsResponsive] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsResponsive(window.innerWidth < 1000);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className={styles.home}>
       <article className={`${styles.full}`}>
-        <div className="titulo">
         <h2 className="font-bold">sofwore Developer</h2>
         <p>
-        Programador con enfoque en desarrollo de aplicaciones web, tengo habilidades tanto en desarrollo backend como frontend.
+          Programador con enfoque en desarrollo de aplicaciones web, tengo habilidades tanto en desarrollo backend como frontend.
         </p>
-        <br />
         <span className="text-primary-color">From:BrianGelvez</span>
-        <div className="flex justify-center items-center gap-5" >
+        <div className={`flex gap-3 ${isResponsive ? 'justify-center items-center' : ''}`}>
           <span className="flex justify-center items-center gap-1">
-            <img className="w-6" src={message} alt="" />
+            <img className="w-5" src={message} alt="" />
             <h5>25</h5>
           </span>
 
           <span className="flex justify-center items-center gap-1">
-            <img className="w-6" src={share} alt="" />
+            <img className="w-5" src={share} alt="" />
             <h5>83</h5>
           </span>
 
           <span className="flex justify-center items-center gap-1">
-            <img className="w-6" src={pin} alt="" />
+            <img className="w-5" src={pin} alt="" />
             <h5>45</h5>
           </span>
-        </div>
         </div>
       </article>
       <article className={`${styles.brian}`}>
