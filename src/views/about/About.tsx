7c4b90fbf7955e7799831524d050b77a86f86ba3
Brian Gelvez <briannn97@gmail.com>
2023-09-11 16:33:36 -0300

    hover image

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -22,8 +22,7 @@ const About: React.FC<AboutProps> = ({ lightMode }) => {
       <div className={styles.aboutMe}>
         <article className={styles.me}>
           <img
-            className={`${styles.img} z-10 m-auto hover:animate-wiggle-more animate-once
-`}
+            className={`${styles.img} z-10 m-auto`}
             src={brian}
             alt=""
           />
