cc8aef10e260132136031a8c9664d7fdf23ab25b
Brian Gelvez <briannn97@gmail.com>
2023-09-26 11:02:00 -0300

    arreglo footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,20 +1,23 @@
-import styles from './footer.module.css'
+import styles from './footer.module.css';
 
 interface FooterProps {
-    lightMode: boolean;
-  }
-
-const Footer: React.FC<FooterProps> = ({lightMode}) => {
-
-    const MI_CONSTANTE = "{Mañana es tarde}";
-
-    return (
-        <header className={`${styles.header} ${lightMode === true ? `${styles.white}` : `${styles.dark}`}`}>
-            <span className='font-bold'>Brian Gelvez Full Stack Developer</span>
-            <span className='font-bold'>Agosto 2023</span>
-            <span className='font-bold'>{MI_CONSTANTE}</span>
-        </header>
-    )
+  lightMode: boolean;
 }
+
+const Footer: React.FC<FooterProps> = ({ lightMode }) => {
+  const MI_CONSTANTE = "{Nunca es tarde}";
+
+  return (
+    <footer
+      className={`${styles.footer} ${
+        lightMode === true ? `${styles.white}` : `${styles.dark}`
+      }`}
+    >
+      <span className='font-bold'>Brian Gelvez Full Stack Developer</span>
+      <span className='font-bold'>Julio 2023</span>
+      <span className='font-bold'>{MI_CONSTANTE}</span>
+    </footer>
+  );
+};
 
 export default Footer;
